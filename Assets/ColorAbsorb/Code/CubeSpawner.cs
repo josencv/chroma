d9b52173f5bbf0fa098c65d2e33d3a9c67fbefb1
GlassToeStudio <GlassToeStudio@gmail.com>
2019-05-27 10:27:35 -0500

    Base chroma shader and test scene.

---
+++ Assets/ColorAbsorb/Code/CubeSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int amount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn(ColorAbsorber.amount));
+    }
+
+    private IEnumerator Spawn(int amount)
+    {
+        int count = 0;
+        while(count < amount)
+        {
+            Quaternion randRot = Quaternion.Euler(new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f)));
+            GameObject go = Instantiate(prefab, transform.position, randRot, this.transform);
+            go.name = "Cube: " + count;
+            count++;
+            yield return new WaitForSeconds(ColorAbsorber.someTime);
+        }
+    }
+}
