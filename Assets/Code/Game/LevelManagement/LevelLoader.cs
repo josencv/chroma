1b9f84ef1b53d0591bedec4f35e2be47320aa4ff
José Ignacio Navarro <josencv@gmail.com>
2019-08-12 21:31:02 -0400

    add enemy model and enemy AI scene

--- Assets/Code/Game/LevelManagement/LevelLoader.cs
+++ Assets/Code/Game/LevelManagement/LevelLoader.cs
@@ -28,13 +28,8 @@ namespace Chroma.Game.LevelManagement
 
         IEnumerator LoadYourAsyncScene(string levelName)
         {
-            // The Application loads the Scene in the background as the current Scene runs.
-            // This is particularly good for creating loading screens.
-            // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
-            // a sceneBuildIndex of 1 as shown in Build Settings.
-
-            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Single);
             loadingScreen.gameObject.SetActive(true);
+            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Single);
 
             // Wait until the asynchronous scene fully loads
             while(!asyncOperation.isDone)
