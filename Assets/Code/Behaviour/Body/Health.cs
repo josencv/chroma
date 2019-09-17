c6db1bd0dbada1778a751bab6dfee93adba6409f
José Ignacio Navarro <josencv@gmail.com>
2019-09-17 19:04:02 -0300

    add health to the enemy and push now deals damage

---
+++ Assets/Code/Behaviour/Body/Health.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Chroma.Behaviour.Body
+{
+
+    public class Health : MonoBehaviour
+    {
+        public event Action Died;
+
+        [SerializeField]
+        private float maxHealth = 100;
+        private float currentHealth;
+
+        private void Awake()
+        {
+            currentHealth = maxHealth;
+        }
+
+        public void ReceiveDamage(float damage)
+        {
+            currentHealth = Mathf.Max(currentHealth - damage, 0);
+            if(currentHealth == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            Debug.Log("Entity has died");
+            Died?.Invoke();
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Code/Behaviour/Skills/Push/Pushable.cs
+++ Assets/Code/Behaviour/Skills/Push/Pushable.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Chroma.Behaviour.Body;
+using Chroma.Game.Configuration;
+using UnityEngine;
 using Zenject;
 
 namespace Chroma.Behaviour.Skills.Push
@@ -6,6 +8,13 @@ namespace Chroma.Behaviour.Skills.Push
     [RequireComponent(typeof(Rigidbody))]
     public class Pushable : MonoBehaviour
     {
+        private const float baseDamage = 5;
+
+        /// <summary>
+        /// The minimum momentum needed for the pushable object to do damage
+        /// </summary>
+        private const float momentumThreshold = 3.0f;
+
         private new Rigidbody rigidbody;
         private bool shouldBePushed;
         private Vector3 force;
@@ -33,6 +42,29 @@ namespace Chroma.Behaviour.Skills.Push
             {
                 rigidbody.AddForce(force);
                 shouldBePushed = false;
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.tag == Tags.Enemy)
+            {
+                Health health = collision.gameObject.GetComponent<Health>();
+                if(health)
+                {
+                    float momentum = rigidbody.mass * rigidbody.velocity.magnitude;
+
+                    if(momentum > momentumThreshold)
+                    {
+                        float damage = baseDamage * momentum;
+                        Debug.Log(collision.gameObject.tag + " received " + damage + " damage from pushable object");
+                        health?.ReceiveDamage(damage);
+                    }
+                    else
+                    {
+                        Debug.Log("Momentum of " + momentum + " was less than the minimum threshold, so no damage was done to " + collision.gameObject.tag);
+                    }
+                }
             }
         }
     }
